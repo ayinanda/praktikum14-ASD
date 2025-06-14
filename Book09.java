e9672b2e269ef2a2aca8dceddb1804482770c764
ayinanda <ayiniswati22@gmail.com>
2025-06-14 21:58:23 +0700

    Class 09

---
+++ Book09.java
@@ -0,0 +1,15 @@
+public class Book09 {
+    public String isbn;
+    public String title;
+
+    public Book09() {
+        
+    }
+    public Book09(String isbn, String title) {
+        this.isbn = isbn;
+        this.title = title;
+    }
+    public String toString() {
+        return "ISBN: " + this.isbn + " Title: " + this.title;
+    }
+}
